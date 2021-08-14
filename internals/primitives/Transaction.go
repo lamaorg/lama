2981708b3527200839017d5a9100a5666d979532
lama <nickllama78@gmail.com>
2021-08-14 17:47:49 -0400

    started transaction logic

--- internals/primitives/Transaction.go
+++ internals/primitives/Transaction.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-var TxType map[string]uint = make(map[string]uint)
+var TxType map[string]uint = make(map[string]uint, 0)
 
 func init() {
 	TxType["COINBASE"] = 0x0000
@@ -19,6 +19,14 @@ func init() {
 	TxType["SYSTEM"] = 0x0999
 	TxType["EVENT"] = 0x0606
 	TxType["REVERT"] = 0x0990
+	TxType["TOKEN"] = 0x0001
+	TxType["VM"] = 0x7777
+	TxType["CONTRACT"] = 0x7771
+	TxType["SWAP"] = 0x7772
+	TxType["EXCHANGE"] = 0x7773
+	TxType["BUY"] = 0x7774
+	TxType["REWARD"] = 0x7775
+
 }
 
 type Transaction struct {
@@ -51,6 +59,7 @@ type Input struct {
 	Time        uint64
 	Amount      *big.Int
 	TxFee       *big.Int
+	State       string
 }
 
 type Output struct {
@@ -59,6 +68,7 @@ type Output struct {
 	Spent    bool
 	Amount   *big.Int
 	Data     []byte
+	State    string
 }
 
 func NewTransaction() {}
