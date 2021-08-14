966cb73a85fa308e8731e9620b99f8c3f4db8c66
lama <nickllama78@gmail.com>
2021-08-14 17:39:53 -0400

    started transaction logic

--- internals/primitives/Transaction.go
+++ internals/primitives/Transaction.go
@@ -1,5 +1,64 @@
 package primitives
 
-type Transaction struct{}
+import (
+	"github.com/lamaorg/lama/common"
+	"math/big"
+)
+
+var TxType map[string]uint = make(map[string]uint)
+
+func init() {
+	TxType["COINBASE"] = 0x0000
+	TxType["WALLET"] = 0x1010
+	TxType["CONTENT"] = 0x0201
+	TxType["MINT"] = 0x0301
+	TxType["BURN"] = 0x0302
+	TxType["TRANSFER"] = 0x0303
+	TxType["ALLOW"] = 0x0401
+	TxType["SECURITY"] = 0x0555
+	TxType["SYSTEM"] = 0x0999
+	TxType["EVENT"] = 0x0606
+	TxType["REVERT"] = 0x0990
+}
+
+type Transaction struct {
+	ID             string
+	Nonce          string
+	Proof          string
+	Hash           string
+	Value          *big.Int
+	Signature      string
+	NumInputs      uint32
+	NumOutputs     uint32
+	Time           uint64
+	Inputs         map[int]*Input
+	Outputs        map[int]*Output
+	Validations    uint64
+	Validated      bool
+	Validator      common.Address
+	ValidatorValue uint64
+}
 
 type Transactions []*Transaction
+
+type Input struct {
+	TxType      uint           `json:"tx_type"`
+	From        common.Address `json:"tx_from"`
+	To          common.Address `json:"tx_to"`
+	IsPayable   bool
+	IsAnonymous bool
+	Data        []byte
+	Time        uint64
+	Amount      *big.Int
+	TxFee       *big.Int
+}
+
+type Output struct {
+	HexInput string
+	To       common.Address
+	Spent    bool
+	Amount   *big.Int
+	Data     []byte
+}
+
+func NewTransaction() {}
